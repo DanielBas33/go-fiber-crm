7257ae4501381961e1fd82274529a2f81c2591ab
Daniel Bas <danielbas@Daniels-MacBook-Pro.local>
2022-04-30 17:26:24 +0200

    CRN using fiber library from Go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DanielBas33/go-fiber-crm/database"
+	"github.com/DanielBas33/go-fiber-crm/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close() // Defer means that this line will execute at the very end of the function
+
+}
